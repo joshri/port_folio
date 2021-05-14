231cea435ac37175fde9298ea28de9cd91e8a1ed
joshri <joshisrael93@gmail.com>
2021-05-14 12:25:31 -0400

    some animation and skill adjustments

--- src/Home.js
+++ src/Home.js
@@ -1,143 +1,162 @@
-import React, { useRef } from 'react';
-import instaImage from './images/glyph-logo_May2016.png';
-import liImage from './images/LI-In-Bug.png';
-import ScrollContainer from './ScrollContainer';
-import ScrollIntoView from 'react-scroll-into-view';
-import { motion } from 'framer-motion';
+import React, { useRef } from "react";
+import instaImage from "./images/glyph-logo_May2016.png";
+import liImage from "./images/LI-In-Bug.png";
+import ScrollContainer from "./ScrollContainer";
+import ScrollIntoView from "react-scroll-into-view";
+import { motion } from "framer-motion";
 
-import { Container, Row, Col, Nav, Image, NavItem } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Image, NavItem } from "react-bootstrap";
 
 function Home(props) {
-	let scalePercent = 0;
-	let navFont = 0;
-	if (window.screen.width > 812) {
-		scalePercent = 350;
-		navFont = '1.8vw';
-	} else {
-		scalePercent = 150;
-		navFont = '1.5vh';
-	}
+  let scalePercent = 0;
+  let navFont = 0;
+  if (window.screen.width > 812) {
+    scalePercent = 350;
+    navFont = "1.8vw";
+  } else {
+    scalePercent = 150;
+    navFont = "1.5vh";
+  }
 
-	const scrollRef = useRef(null);
+  const scrollRef = useRef(null);
 
-	return (
-		<motion.div
-			style={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ duration: 1 }}>
-			<Container fluid>
-				<Row style={{ minHeight: '100%' }}>
-					<Col
-						className='sidebar'
-						sm='auto'
-						style={{
-							borderRight: '5px solid black',
-							minHeight: '110vh',
-							padding: '2px',
-							width: '15%',
-						}}>
-						<div style={{ height: '15vh' }}></div>
-						<Nav
-							style={{ height: '85vh' }}
-							variant='pills'
-							className='flex-column'>
-							<NavItem
-								onClick={() => {
-									scrollRef.current.scrollTo({
-										top: 0,
-										behavior: 'smooth',
-									});
-								}}
-								style={{ fontSize: navFont }}>
-								About
-							</NavItem>
-							<ScrollIntoView selector='#resume'>
-								<NavItem style={{ fontSize: navFont }}>Resume</NavItem>
-							</ScrollIntoView>
-							<ScrollIntoView selector='#projects'>
-								<NavItem style={{ fontSize: navFont }}>Projects:</NavItem>
-							</ScrollIntoView>
+  return (
+    <motion.div
+      style={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <Container fluid>
+        <Row style={{ minHeight: "100%" }}>
+          <Col
+            className="sidebar"
+            sm="auto"
+            style={{
+              borderRight: "5px solid black",
+              minHeight: "110vh",
+              padding: "2px",
+              width: "15%",
+            }}
+          >
+            <div style={{ height: "15vh" }}></div>
+            <Nav
+              style={{ height: "85vh" }}
+              variant="pills"
+              className="flex-column"
+            >
+              <NavItem
+                onClick={() => {
+                  scrollRef.current.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                  });
+                }}
+                style={{ fontSize: navFont }}
+              >
+                About
+              </NavItem>
+              <ScrollIntoView selector="#resume">
+                <NavItem style={{ fontSize: navFont }}>Resume</NavItem>
+              </ScrollIntoView>
+              <ScrollIntoView selector="#projects">
+                <NavItem style={{ fontSize: navFont }}>Projects:</NavItem>
+              </ScrollIntoView>
 
-							<NavItem
-								className='navItem2'
-								style={{ fontSize: navFont, marginTop: '5px' }}>
-								- For All Seasons
-							</NavItem>
+              <NavItem
+                className="navItem2"
+                style={{ fontSize: navFont, marginTop: "5px" }}
+              >
+                - For All Seasons
+              </NavItem>
 
-							<NavItem className='navItem2' style={{ fontSize: navFont }}>
-								- Funky Simon
-							</NavItem>
+              <NavItem className="navItem2" style={{ fontSize: navFont }}>
+                - Funky Simon
+              </NavItem>
 
-							<NavItem style={{ fontSize: navFont }} className='navItem2'>
-								- Is It Hot?
-							</NavItem>
-						</Nav>
-					</Col>
+              <NavItem style={{ fontSize: navFont }} className="navItem2">
+                - Is It Hot?
+              </NavItem>
+            </Nav>
+          </Col>
 
-					<div
-						ref={scrollRef}
-						className='pleaseScroll'
-						style={{
-							display: 'inline-block',
-							padding: 0,
-							maxHeight: '110vh',
-							maxWidth: '85vw',
-							overflow: 'scroll',
-						}}>
-						<div className='topContainer'>
-							<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-								<h1
-									style={{
-										fontSize: `${scalePercent}%`,
-										textAlign: 'center',
-										zIndex: 70,
-									}}
-									className='name'>
-									JOSHUA ISRAEL
-								</h1>
-								<h6 style={{fontSize: navFont, textAlign: 'center'}}>FRONT-END / FULL-STACK DEVELOPER</h6>
-							</div>
-							<div className='contact'>
-								<div className='socialDiv'>
-									<a
-										target='_blank'
-										rel='noopener noreferrer'
-										href='https://www.linkedin.com/in/joshri'>
-										<Image className='social' src={liImage} />
-									</a>
+          <div
+            ref={scrollRef}
+            className="pleaseScroll"
+            style={{
+              display: "inline-block",
+              padding: 0,
+              maxHeight: "110vh",
+              maxWidth: "85vw",
+              overflow: "scroll",
+            }}
+          >
+            <div className="topContainer">
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <h1
+                  style={{
+                    fontSize: `${scalePercent}%`,
+                    textAlign: "center",
+                    zIndex: 70,
+                  }}
+                  className="name"
+                >
+                  JOSHUA ISRAEL
+                </h1>
+                <h6 style={{ fontSize: navFont, textAlign: "center" }}>
+                  FRONT-END DEVELOPER
+                </h6>
+              </div>
+              <div className="contact">
+                <div className="socialDiv">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.linkedin.com/in/joshri"
+                  >
+                    <Image className="social" src={liImage} />
+                  </a>
 
-									<a
-										target='_blank'
-										rel='noopener noreferrer'
-										href='https://www.instagram.com/sirjoshisrael/?hl=en'>
-										<Image className='social' src={instaImage} />
-									</a>
-								</div>
-								<div style={{ display: 'flex' }}>
-									<svg
-										width='1em'
-										height='1em'
-										viewBox='0 0 16 16'
-										className='bi bi-envelope'
-										fill='currentColor'
-										xmlns='http://www.w3.org/2000/svg'>
-										<path
-											fillRule='evenodd'
-											d='M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z'
-										/>
-									</svg>
-									<h6 style={{ marginLeft: '5px', fontSize: navFont }}>
-										joshisrael93@gmail.com
-									</h6>
-								</div>
-							</div>
-						</div>
-						<ScrollContainer />
-					</div>
-				</Row>
-			</Container>
-		</motion.div>
-	);
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.instagram.com/sirjoshisrael/?hl=en"
+                  >
+                    <Image className="social" src={instaImage} />
+                  </a>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    className="bi bi-envelope"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"
+                    />
+                  </svg>
+                  <h6 style={{ marginLeft: "5px", fontSize: navFont }}>
+                    joshisrael93@gmail.com
+                  </h6>
+                </div>
+              </div>
+            </div>
+            <ScrollContainer />
+          </div>
+        </Row>
+      </Container>
+    </motion.div>
+  );
 }
 
 export default Home;
--- src/Title.js
+++ src/Title.js
@@ -1,130 +1,153 @@
-import React, { useState, useRef } from 'react';
-import title_photo from './images/TitlePhoto2.jpg';
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import React, { useState, useRef } from "react";
+import title_photo from "./images/TitlePhoto2.jpg";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 function Title(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [finished, setFinished] = useState("none");
+  setTimeout(() => setFinished("flex"), 24000);
 
-	let [loaded, setLoaded] = useState(false);
-	let [finished, setFinished] = useState('none');
-	setTimeout(() => setFinished('flex'), 24000);
+  const audioTag = useRef(null);
 
-	const audioTag = useRef(null);
+  return (
+    <div
+      onLoad={window.scrollTo(0, 1)}
+      className="titleContainer"
+      style={{ height: props.adjustVh * 100 }}
+    >
+      <audio
+        id="audio"
+        ref={audioTag}
+        preload
+        onCanPlay={() => {
+          setLoaded(true);
+          audioTag.current.play();
+        }}
+        src={
+          "https://raw.githubusercontent.com/joshri/port_folio/master/src/audio/PortfolioThemeLowest.mp3"
+        }
+      />
+      {!loaded ? (
+        <div
+          style={{
+            width: "100%",
+            height: props.adjustVh * 100,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            margin: 0,
+          }}
+        >
+          <h1>LOADING</h1>
+          <h6>Stuck? Give it a second.</h6>
+          <h6>
+            On mobile? Try this?{" "}
+            <Button
+              variant="outline-dark"
+              size="sm"
+              onClick={() => {
+                setLoaded(true);
+                audioTag.current.play();
+              }}
+            >
+              play?
+            </Button>
+          </h6>
+          <h6 style={{ textDecoration: "underline" }}>
+            <Link to="/">back to the start</Link>
+          </h6>
+          <h6 style={{ textDecoration: "underline" }}>
+            <Link to="/home">onward to the portfolio</Link>
+          </h6>
+        </div>
+      ) : (
+        <div>
+          <motion.div
+            animate={{ borderColor: "black", opacity: 1 }}
+            transition={{ duration: 2 }}
+            className="row titleEdge"
+            style={{
+              borderBottom: "5px solid #ffcf3d",
+              opacity: 0,
+              height: props.adjustVh * 12.5,
+            }}
+          >
+            <h1 className="col zoom">MEET</h1>
+            <h1 className="col zoom forYour">YOUR</h1>
+            <h1 className="col zoom forNew">NEW</h1>
+          </motion.div>
 
-	return (
-		<div onLoad={window.scrollTo(0, 1)} className='titleContainer' style={{height: props.adjustVh * 100}}>
-			<audio
-				id='audio'
-				ref={audioTag}
-				preload
-				onCanPlay={() => {
-					setLoaded(true);
-					audioTag.current.play();
-				}}
-				src={
-					'https://raw.githubusercontent.com/joshri/port_folio/master/src/audio/PortfolioThemeLowest.mp3'
-				}
-			/>
-			{!loaded ? (
-				<div
-					style={{
-						width: '100%',
-						height: props.adjustVh * 100,
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'center',
-						textAlign: 'center',
-						margin: 0,
-					}}>
-					<h1>LOADING</h1>
-					<h6>Stuck? Give it a second.</h6>
-					<h6>
-						On mobile? Try this?{' '}
-						<Button variant='outline-dark' size='sm'
-							onClick={() => {
-								setLoaded(true);
-								audioTag.current.play();
-							}}>
-							play?
-						</Button>
-					</h6>
-					<h6 style={{ textDecoration: 'underline' }}>
-						<Link to='/'>back to the start</Link>
-					</h6>
-					<h6 style={{ textDecoration: 'underline' }}>
-						<Link to='/home'>onward to the portfolio</Link>
-					</h6>
-				</div>
-			) : (
-				<div>
-					<motion.div
-						animate={{ borderColor: 'black', opacity: 1 }}
-						transition={{ duration: 2 }}
-						className='row titleEdge'
-						style={{ borderBottom: '5px solid #ffcf3d', opacity: 0, height: props.adjustVh * 12.5 }}>
-						<h1 className='col zoom'>MEET</h1>
-						<h1 className='col zoom forYour'>YOUR</h1>
-						<h1 className='col zoom forNew'>NEW</h1>
-					</motion.div>
-
-					<div className='nameContainer' style={{height: props.adjustVh * 75}}>
-						<motion.div
-							animate={{ scale: 3, opacity: 0 }}
-							transition={{ delay: 17.5, duration: 0.5 }}
-							className='skillsContainer'>
-							<div className='row skillsRow'>
-								<h3 className='col skillsItem g'>HTML</h3>
-								<h3 className='col skillsItem c'>CSS</h3>
-								<h3 className='col skillsItem d' style={{ fontSize: '3.5vw' }}>
-									BOOTSTRAP
-								</h3>
-								<h3 className='col skillsItem h'>SASS</h3>
-							</div>
-							<div className='row skillsRow'>
-								<h3 className='col skillsItem a'>JAVASCRIPT</h3>
-								<h3 className='col skillsItem b'>REACT</h3>
-							</div>
-							<div className='row skillsRow'>
-								<h3 className='col skillsItem e'>EXPRESS</h3>
-								<h3 className='col skillsItem i'>PYTHON</h3>
-								<h3 className='col skillsItem f'>AWS</h3>
-							</div>
-						</motion.div>
-						<motion.img
-							animate={{ width: '75%', height: '75%', opacity: 1 }}
-							transition={{ delay: 20, duration: 4 }}
-							alt={'a handsome image of Joshua Israel'}
-							src={title_photo}
-							className='image'></motion.img>
-						<Link className='link' to='/home'>
-							<motion.Button
-								style={{ display: finished, fontSize: '175%' }}
-								className='link'
-								animate={{ opacity: [0, 1], display: 'flex' }}
-								transition={{ duration: 2, delay: 24 }}
-								variant='danger'>
-								ENTER
-							</motion.Button>
-						</Link>
-						<h1 className='title'>JOSHUA</h1>
-						<h1 className='title' style={{ animationDelay: '18.5s' }}>
-							ISRAEL
-						</h1>
-					</div>
-					<motion.div
-						animate={{ borderColor: 'black', opacity: 1 }}
-						transition={{ duration: 2 }}
-						style={{ borderTop: '5px solid #ffcf3d', opacity: 0, height: props.adjustVh * 12.5 }}
-						className='row titleEdge'>
-						<h1 className='col zoom forBot'>SOFTWARE</h1>
-						<h1 className='col zoom forBot'> ENGINEER</h1>
-					</motion.div>
-				</div>
-			)}
-		</div>
-	);
+          <div
+            className="nameContainer"
+            style={{ height: props.adjustVh * 75 }}
+          >
+            <motion.div
+              animate={{ scale: 3, opacity: 0 }}
+              transition={{ delay: 17.5, duration: 0.5 }}
+              className="skillsContainer"
+            >
+              <div className="row skillsRow">
+                <h3 className="col skillsItem g" style={{ fontSize: "3.5vw" }}>
+                  BOOTSTRAP
+                </h3>
+                <h3 className="col skillsItem c">HTML</h3>
+                <h3 className="col skillsItem d">CSS</h3>
+                <h3 className="col skillsItem h">SASS</h3>
+              </div>
+              <div className="row skillsRow">
+                <h3 className="col skillsItem a">JAVASCRIPT</h3>
+                <h3 className="col skillsItem b">REACT / NATIVE</h3>
+              </div>
+              <div className="row skillsRow">
+                <h3 className="col skillsItem e">NODE</h3>
+                <h3 className="col skillsItem i">PYTHON</h3>
+                <h3 className="col skillsItem f">AWS</h3>
+              </div>
+            </motion.div>
+            <motion.img
+              animate={{ width: "75%", height: "75%", opacity: 1 }}
+              transition={{ delay: 20, duration: 4 }}
+              alt={"a handsome image of Joshua Israel"}
+              src={title_photo}
+              className="image"
+            ></motion.img>
+            <Link className="link" to="/home">
+              <motion.Button
+                style={{ display: finished, fontSize: "175%" }}
+                className="link"
+                animate={{ opacity: [0, 1], display: "flex" }}
+                transition={{ duration: 2, delay: 24 }}
+                variant="danger"
+              >
+                ENTER
+              </motion.Button>
+            </Link>
+            <h1 className="title">JOSHUA</h1>
+            <h1 className="title" style={{ animationDelay: "18.5s" }}>
+              ISRAEL
+            </h1>
+          </div>
+          <motion.div
+            animate={{ borderColor: "black", opacity: 1 }}
+            transition={{ duration: 2 }}
+            style={{
+              borderTop: "5px solid #ffcf3d",
+              opacity: 0,
+              height: props.adjustVh * 12.5,
+            }}
+            className="row titleEdge"
+          >
+            <h1 className="col zoom forBot">SOFTWARE</h1>
+            <h1 className="col zoom forBot"> ENGINEER</h1>
+          </motion.div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Title;
