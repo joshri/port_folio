fb21af6737ce129b22d2dbb32f0448e5044638fe
joshri <joshisrael93@gmail.com>
2021-07-08 15:36:34 -0400

    initial name layer animation

--- src/Title.js
+++ src/Title.js
@@ -3,19 +3,21 @@ import { NavLink } from "react-router-dom";
 import title_photo from "./assets/images/TitlePhoto2.jpg";
 import { motion } from "framer-motion";
 
+import { createNameLayer } from "./utils";
+
 function Title(props) {
   let [loaded, setLoaded] = useState(false);
 
   const audioTag = useRef(null);
 
   return (
-    <section onLoad={window.scrollTo(0, 1)} className="titleContainer">
+    <section className="titleContainer">
       <audio
         id="audio"
         ref={audioTag}
         preload="auto"
         onCanPlay={() => {
-          // setLoaded(true);
+          setLoaded(true);
           // audioTag.current.play();
         }}
         src={
@@ -41,83 +43,48 @@ function Title(props) {
           <NavLink to="/home">onward to the portfolio</NavLink>
         </div>
       ) : (
-        <div>
-          <motion.div
-            animate={{ borderColor: "black", opacity: 1 }}
-            transition={{ duration: 2 }}
-            className="row titleEdge"
-            style={{
-              borderBottom: "5px solid #ffcf3d",
-              opacity: 0,
-            }}
-          >
-            <h1 className="col zoom">MEET</h1>
-            <h1 className="col zoom forYour">YOUR</h1>
-            <h1 className="col zoom forNew">NEW</h1>
-          </motion.div>
-
-          <div
-            className="nameContainer"
-            style={{ height: props.adjustVh * 75 }}
-          >
-            <motion.div
-              animate={{ scale: 3, opacity: 0 }}
-              transition={{ delay: 17.5, duration: 0.5 }}
-              className="skillsContainer"
+        <div className="column">
+          {/* top */}
+          <div className="titleEdge" style={{ borderTop: "none" }}></div>
+          {/* center */}
+          <div>
+            {/* name layer */}
+            <div
+              style={{
+                top: "12.5vh",
+                height: "75vh",
+                width: "100vw",
+                background: "transparent",
+              }}
             >
-              <div className="row skillsRow">
-                <h3 className="col skillsItem g" style={{ fontSize: "3.5vw" }}>
-                  BOOTSTRAP
-                </h3>
-                <h3 className="col skillsItem c">HTML</h3>
-                <h3 className="col skillsItem d">CSS</h3>
-                <h3 className="col skillsItem h">SASS</h3>
-              </div>
-              <div className="row skillsRow">
-                <h3 className="col skillsItem a">JAVASCRIPT</h3>
-                <h3 className="col skillsItem b">REACT / NATIVE</h3>
-              </div>
-              <div className="row skillsRow">
-                <h3 className="col skillsItem e">NODE</h3>
-                <h3 className="col skillsItem i">PYTHON</h3>
-                <h3 className="col skillsItem f">AWS</h3>
-              </div>
-            </motion.div>
-            <motion.img
-              animate={{ width: "75%", height: "75%", opacity: 1 }}
-              transition={{ delay: 20, duration: 4 }}
-              alt={"a handsome image of Joshua Israel"}
-              src={title_photo}
-              className="image"
-            ></motion.img>
-            <NavLink to="/home">
-              <motion.Button
-                style={{ display: "none", fontSize: "175%" }}
-                className="link"
-                animate={{ opacity: [0, 1], display: "flex" }}
-                transition={{ duration: 2, delay: 24 }}
-              >
-                ENTER
-              </motion.Button>
-            </NavLink>
-            <h1 className="title">JOSHUA</h1>
-            <h1 className="title" style={{ animationDelay: "18.5s" }}>
-              ISRAEL
-            </h1>
+              {createNameLayer().map((style) => {
+                return (
+                  <motion.h1
+                    style={{
+                      position: "absolute",
+                      top: `${style.top}vh`,
+                      left: `${style.left}vh`,
+                      fontSize: style.fontSize,
+                      color: style.color,
+                      opacity: 0,
+                    }}
+                    animate={{ opacity: [0, 1, 0], rotate: style.rotate }}
+                    transition={{
+                      delay: style.animationDelay,
+                      duration: style.animationDuration,
+                    }}
+                  >
+                    JOSHUA ISRAEL
+                  </motion.h1>
+                );
+              })}
+            </div>
           </div>
-          <motion.div
-            animate={{ borderColor: "black", opacity: 1 }}
-            transition={{ duration: 2 }}
-            style={{
-              borderTop: "5px solid #ffcf3d",
-              opacity: 0,
-              height: props.adjustVh * 12.5,
-            }}
-            className="row titleEdge"
-          >
-            <h1 className="col zoom forBot">SOFTWARE</h1>
-            <h1 className="col zoom forBot"> ENGINEER</h1>
-          </motion.div>
+          {/* bottom */}
+          <div
+            className="titleEdge"
+            style={{ borderBottom: "none", top: "87.5vh" }}
+          ></div>
         </div>
       )}
     </section>
---
+++ src/utils.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const createNameLayer = () => {
+  let nameArray = [];
+  const colorOptions = {
+    0: "#b48d18",
+    1: "#3daeff",
+    2: "#bf2ed6",
+    3: "#99d62e",
+    4: "#d66b2e",
+  };
+  for (let i = 0; i <= 75; i++) {
+    let style = {
+      fontSize: 0,
+      color: 0,
+      animationDuration: 0,
+      animationDelay: 0,
+      rotate: 0,
+      top: 0,
+      left: 0,
+    };
+    style.fontSize = Math.floor(Math.random() * 90) + 12;
+    style.color = colorOptions[Math.floor(Math.random() * 5)];
+    style.animationDuration = Math.floor(Math.random() * 4) + 1;
+    style.animationDelay = Math.random() * 4;
+    const rotateNumber = Math.floor(Math.random() * 5);
+    if (rotateNumber === 0) {
+      style.rotate = -(Math.floor(Math.random() * 360) + 1);
+    } else if (rotateNumber === 1) {
+      style.rotate = Math.floor(Math.random() * 360) + 1;
+    }
+    style.top = Math.floor(Math.random() * 65) + 13.5;
+    style.left = Math.floor(Math.random() * 40);
+    nameArray.push(style);
+  }
+  return nameArray;
+};
