8980925354f07ec369e7a2af5d6098f6feff5af9
joshri <joshisrael93@gmail.com>
2021-07-19 18:04:35 -0400

    about and resume, resized bartab videos

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,12 @@
 import React from "react";
 import liImage from "./assets/images/LI-In-Bug.png";
 import ScrollContainer from "./ScrollContainer";
-import { motion } from "framer-motion";
 
 function Home(props) {
   return (
-    <motion.section
-      style={{ opacity: 0, flexDirection: "row" }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-    >
+    <section style={{ flexDirection: "row", background: "transparent" }}>
       {/* sidebar */}
-      <div
-        style={{
-          borderRight: "5px solid black",
-          minHeight: "100vh",
-          minWidth: "10%",
-        }}
-      >
+      <div className="homeSidebar">
         <div
           style={{
             minWidth: "80%",
@@ -41,9 +30,9 @@ function Home(props) {
             <h1 style={{ width: "100%" }}>JOSHUA ISRAEL</h1>
             <h6 style={{ width: "100%" }}>FRONT-END DEVELOPER</h6>
           </div>
-          <div style={{ width: "30%" }}>
+          <div style={{ width: "40%", justifyContent: "center" }}>
             <svg
-              width="2vw"
+              width="2.5vw"
               height="50%"
               viewBox="0 0 16 16"
               className="bi bi-envelope"
@@ -55,7 +44,7 @@ function Home(props) {
                 d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"
               />
             </svg>
-            <h6>joshisrael93@gmail.com</h6>
+            <h6 style={{ marginLeft: "5px" }}>joshisrael93@gmail.com</h6>
           </div>
 
           <a
@@ -74,7 +63,7 @@ function Home(props) {
         {/* content */}
         <ScrollContainer />
       </div>
-    </motion.section>
+    </section>
   );
 }
 
--- src/ScrollContainer.js
+++ src/ScrollContainer.js
@@ -9,14 +9,21 @@ import IsItHot from "./assets/video/IsItHotGif.mp4";
 import Sidecar from "./assets/video/SidecarDemoMovie.mp4";
 import BartabOpen from "./assets/video/BartabOpenFinal3.mov";
 import BartabUpdate from "./assets/video/BartabUpdateGif.mp4";
+import { withRouter } from "react-router";
 
 function ScrollContainer(props) {
   let [resFull, setResFull] = useState("none");
 
   return (
     <div
-      className="column"
-      style={{ width: "100%", height: "100%", overflow: "scroll" }}
+      className="column min-width"
+      style={{
+        minWidth: "90vw",
+        minHeight: "90vh",
+        marginTop: "10vh",
+        overflowY: "scroll",
+        overflowX: "hidden",
+      }}
     >
       {/* resume modal TODO: link to Google Doc or Download */}
       <div
@@ -53,154 +60,152 @@ function ScrollContainer(props) {
         </div>
       </div>
       {/* background layer */}
-      <div style={{ position: "absolute", top: "20%", left: "10%", zIndex: 0 }}>
-        <h1 style={{ transform: "rotate(90deg)", fontSize: "100px" }}>ABOUT</h1>
+      <div
+        className="column min-width"
+        style={{ minHeight: "100%", width: "100%" }}
+      >
+        <h1
+          className="about"
+          style={{
+            transform: "rotate(90deg)",
+            marginTop: "70%",
+            marginRight: "70%",
+          }}
+        >
+          ABOUT
+        </h1>
+        <div style={{ minHeight: "40vh" }} />
+        <h1 className="resume" style={{}}>
+          RESUME
+        </h1>
+
+        <h1 style={{ letterSpacing: "5vw" }}>BARTAB</h1>
       </div>
       {/* foreground layer */}
       <div
-        className="column"
+        className="column min-width"
         style={{
           position: "relative",
-          background: "transparent",
-          zIndex: 3,
+          bottom: "",
+          minHeight: "100%",
+          width: "100%",
+          zIndex: 2,
         }}
       >
-        <div className="scrollItem">
+        <div
+          className="has-background column"
+          style={{
+            padding: "3%",
+            height: "80vh",
+            minWidth: "250px",
+            width: "50%",
+            border: "3px solid black",
+            borderRadius: "20px",
+            zIndex: 4,
+            marginLeft: "20vw",
+          }}
+        >
           <div
+            className="column"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "20vh",
-            }}
-          >
-            <p
-              style={{
-                opacity: 1,
-
-                width: "50%",
-                padding: "15px",
-                border: "2px solid black",
-              }}
-            >
-              Welcome to my Internet House! You've got everything you could ever
-              want right here at your fingertips. The only thing that's missing
-              is another look at that photo from the intro, which I am happy to
-              send on request.
-              <br></br>
-              <br></br>Seriously though - I love creating, I love learning, and
-              I used to be paid to sing and dance in front of thousands of
-              people eight times a week - or as I like to call it now, "giving
-              presentations".<br></br>
-              <br></br>Yes, I actually like talking to people and giving
-              presentations. So if you need that, Or:<br></br>
-              <br></br>
-              React (Redux, Hooks), React Native (XCode, Android Studio),
-              Javascript, CSS (SASS, Styled Components, Bootstrap), HTML, or
-              perhaps even a little Python or Node...
-              <br></br>
-              <br></br>Let's make something together!<br></br>
-              <br></br>
-            </p>
-          </div>
-        </div>
-
-        <div className="scrollItem">
-          <h1 className="scrollTitle" style={{}}>
-            RESUME
-          </h1>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              alignItems: "center",
-              marginTop: "20px",
-            }}
-          >
-            <svg
-              width="30%"
-              height="30%"
-              viewBox="0 0 16 16"
-              className="bi bi-arrow-right"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-              />
-            </svg>
-            <img
-              alt="resume thumbnail"
-              className="resumeThumb"
-              style={{ cursor: "pointer" }}
-              src={thumbnailPic}
-              onClick={() => setResFull("block")}
-            />
-          </div>
-        </div>
-
-        <div className="scrollItem">
-          <h1
-            style={{
-              marginBottom: "100px",
-            }}
-            className="scrollTitle"
-          >
-            BARTAB
-          </h1>
-          <div
-            style={{
-              display: "flex",
+              height: "100%",
               width: "100%",
-              justifyContent: "space-evenly",
             }}
           >
-            {" "}
-            {/* <video
-              title={props.title}
-              src={BartabOpen}
-              type="video/mov"
-              autoPlay
-              loop
-              className="giphy-embed"
-              allowFullScreen
-              muted
-            ></video>
-            <video
-              title={props.title}
-              src={BartabUpdate}
-              type="video/mp4"
-              autoPlay
-              loop
-              className="giphy-embed"
-              allowFullScreen
-              muted
-            ></video>
+            <h6 className="about">
+              Welcome to the #1 Website In The World For All Things Related To
+              Me, Joshua Israel!
+            </h6>
+            <h6 className="about">
+              Once, in an interview, I was asked, "Are you always this happy
+              when you code?"
+            </h6>
+            <h6 className="about">I said, "Yes."</h6>
+            <h6 className="about">
+              I also said, "That's going right on my website."
+            </h6>
+            <h6 className="about">
+              From 2016-2020, I was paid to sing and dance live in front of
+              thousands of people eight times a week - or as I like to call it
+              now, "giving presentations". Now, I use the same creativity and
+              energy to create a slightly different user-facing experience. I
+              use Javascript instead of my actual human body, but it's basically
+              the same.
+            </h6>
+            <h6 className="about">
+              I've got React (Redux, Hooks), React Native (XCode, Android
+              Studio), Javascript (ES6), CSS (SASS, Styled Components,
+              Bootstrap), HTML, and a dash of Python and Node in my toolbox...SO
+              FAR. I used to be an actor okay? I'm willing to learn something
+              new!
+            </h6>
+            <h6 className="about">
+              I am, of course, happy to send a copy of the photo at the end of
+              the party animation upon request.
+            </h6>
           </div>
-          <div>
-            <video
-              title={props.title}
-              src={Sidecar}
-              type="video/mp4"
-              autoPlay
-              loop
-              className="giphy-embed"
-              allowFullScreen
-              muted
-            ></video> */}
-          </div>
-          <p
-            style={{
-              opacity: 1,
+        </div>
+        <div style={{ height: "20vh" }} />
+        <div>
+          <svg
+            width="30vw"
+            height="30vh"
+            viewBox="0 0 16 16"
+            className="bi bi-arrow-right"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+            />
+          </svg>
+          <img
+            alt="resume thumbnail"
+            className="resumeThumb"
+            style={{ cursor: "pointer" }}
+            src={thumbnailPic}
+            onClick={() => setResFull("block")}
+          />
+        </div>
+      </div>
+      <div style={{ height: "50vh" }}>
+        <video
+          style={{ width: "50%", height: "100%" }}
+          title={props.title}
+          src={BartabOpen}
+          type="video/mov"
+          autoPlay
+          loop
+          className="giphy-embed"
+          allowFullScreen
+          muted
+        ></video>
+        <video
+          style={{ width: "50%", height: "100%" }}
+          title={props.title}
+          src={BartabUpdate}
+          type="video/mp4"
+          autoPlay
+          loop
+          className="giphy-embed"
+          allowFullScreen
+          muted
+        ></video>
+      </div>
+      <video
+        style={{ width: "100%", height: "30vh" }}
+        title={props.title}
+        src={Sidecar}
+        type="video/mp4"
+        autoPlay
+        loop
+        className="giphy-embed"
+        allowFullScreen
+        muted
+      ></video>
 
-              width: "50%",
-              padding: "15px",
-              border: "2px solid black",
-            }}
-          >
-            January 2021 - Present <br></br>
-          </p>
+      {/* 
         </div>
         <div style={{ justifyContent: "center", alignItems: "center" }}>
           <h1
@@ -211,7 +216,7 @@ function ScrollContainer(props) {
           >
             PROJECTS
           </h1>
-          <div id="forallseasons"></div>
+          
           <Project
             title="FOR ALL SEASONS"
             deployed="https://for-all-seasons.herokuapp.com"
@@ -251,8 +256,7 @@ function ScrollContainer(props) {
               "Styled Components for a reusable button that refreshes the API call.",
             ]}
           />
-        </div>
-      </div>
+        </div> */}
     </div>
   );
 }
