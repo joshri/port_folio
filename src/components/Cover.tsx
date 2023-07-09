eb56673f5b9b2cd1a2eca36d2f1a6f8863ce16cf
Joshua Israel <joshisrael93@gmail.com>
2023-07-09 17:02:16 -0400

    initial text animations + confetti

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,22 +1,39 @@
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import coverImg from "../assets/TitlePhoto.jpg";
-import { Container } from "../styles";
+import { Container, createConfetti } from "../styles";
 
-const imageOut = {
-  width: "200%",
-  height: "auto",
-  opacity: 0,
-};
-
-const CoverImg = styled.img`
-  width: 100%;
-  height: auto;
-`;
+const CoverImg = styled(motion.img)``;
 
 const CoverButton = styled.button`
+  background: transparent;
   position: absolute;
-  margin: 0 auto;
+  top: 50%;
+  left: 50%;
+  padding: 10px;
+  border: 3px solid white;
+  border-radius: 10px;
+  letter-spacing: 5px;
+  font-size: 48px;
+  box-shadow: 0 0 2em 0.2em white;
+`;
+
+const CoverTitle = styled(motion.h1)`
+  font-size: 150px;
+  letter-spacing: 2px;
+  position: absolute;
+`;
+
+const ConfettiContainer = styled(motion.div)`
+  height: 10px;
+  width: 30px;
+  position: absolute;
+  top: 100vh;
+  right: 100vw;
+  &.confetti-left {
+    right: 0vw;
+  }
 `;
 
 type Props = {
@@ -25,18 +42,73 @@ type Props = {
 
 function Cover({ className }: Props) {
   return (
-    <Container className={className} exit={imageOut}>
-      <CoverImg src={coverImg} />
+    <Container
+      className={className}
+      exit={{
+        scale: 4,
+        opacity: 0,
+      }}
+      transition={{ duration: 2 }}
+    >
+      <CoverTitle
+        //flex centered spacing places the "0" position at dead center. Animate around "50" === "100"
+        initial={{ x: "-80vw", y: "-40vh" }}
+        animate={{ x: "-25vw" }}
+        transition={{ delay: 0.5, duration: 2 }}
+      >
+        THE ONE
+      </CoverTitle>
+      <CoverTitle
+        initial={{ x: "80vw", y: "-40vh" }}
+        animate={{ x: "25vw" }}
+        transition={{ delay: 2.5, duration: 2 }}
+      >
+        THE ONLY
+      </CoverTitle>
+      <CoverTitle
+        initial={{ rotate: "180deg", y: "60vh" }}
+        animate={{ rotate: "2160deg", y: "40vh" }}
+        transition={{ delay: 4.5, duration: 2, ease: "easeOut" }}
+      >
+        JOSHUA ISRAEL
+      </CoverTitle>
+      <CoverImg
+        src={coverImg}
+        initial={{ height: 0, width: 0 }}
+        animate={{ width: "100%", height: "100%" }}
+        transition={{ duration: 6, type: "tween", ease: "easeIn" }}
+      />
       <CoverButton>
         <Link to="/home">Enter</Link>
       </CoverButton>
+      {createConfetti().map((style, index) => {
+        return (
+          <ConfettiContainer
+            className={index > 40 ? "confetti-left" : ""}
+            key={index}
+            animate={{
+              x: `${index > 40 ? -style.translateX : style.translateX}vw`,
+              y: `-${style.translateY}vh`,
+              rotate: style.rotate,
+            }}
+            transition={{
+              duration: 3,
+              delay: 6,
+              repeat: 0,
+            }}
+            style={{
+              background: style.background,
+            }}
+          />
+        );
+      })}
     </Container>
   );
 }
 
 export default styled(Cover).attrs({ className: Cover.name })`
+  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
 `;
--- src/styles.tsx
+++ src/styles.tsx
@@ -9,7 +9,12 @@ export const GlobalStyles = createGlobalStyle`
         max-width: 100vw;
         padding: 0;
         margin: 0;
-        background: #966919;
+        background: black;
+    }
+
+    h1, h2, h3, h4, h5, h6, p, span {
+      font-family: 'Lato', sans-serif;
+      color: white;
     }
 `;
 
@@ -17,3 +22,32 @@ export const Container = styled(motion.div)`
   width: 100%;
   height: 100%;
 `;
+
+type ConfettiStyle = {
+  background: string;
+  translateX: number;
+  translateY: number;
+  rotate: number;
+};
+
+export const createConfetti = (): ConfettiStyle[] => {
+  let confettiArray = [];
+  const colorOptions = ["#b48d18", "#3daeff", "#bf2ed6", "#99d62e", "#d66b2e"];
+  for (let i = 0; i <= 80; i++) {
+    let style: ConfettiStyle = {
+      background: "",
+      translateX: 0,
+      translateY: 0,
+      rotate: 0,
+    };
+    let x = Math.floor(Math.random() * 40) + 105;
+    let y = Math.floor(Math.random() * 87.5) + 50;
+    let rotate = Math.floor(Math.random() * 360) + 360;
+    style.background = colorOptions[Math.floor(Math.random() * 5)];
+    style.translateX = x;
+    style.translateY = y;
+    style.rotate = rotate;
+    confettiArray.push(style);
+  }
+  return confettiArray;
+};
